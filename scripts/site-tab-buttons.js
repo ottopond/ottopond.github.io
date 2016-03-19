0bd2b0cf6e9a362b954a5083dcfa3a2b533efdf6
Geoff <geoff@green-pedals.com>
2016-03-19 15:48:14 -0400

    clean up html

---
+++ scripts/site-tab-buttons.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+//THIS IS THE SCRIPT THAT MAKES THE SUB-TAB BUTTONS WORK
+
+function MM_showHideLayers() { //v9.0
+  var i,p,v,obj,args=MM_showHideLayers.arguments;
+  for (i=0; i<(args.length-2); i+=3) 
+  with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
+    if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
+    obj.visibility=v; }
+}
+function MM_goToURL() { //v3.0
+  var i, args=MM_goToURL.arguments; document.MM_returnValue = false;
+  for (i=0; i<(args.length-1); i+=2) eval(args[i]+".location='"+args[i+1]+"'");
+}
