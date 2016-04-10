2694ad52b01b9632411f92dcf1f27801c0db2697
Geoff <geoff@green-pedals.com>
2016-04-10 17:00:26 -0400

    carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,6 +1,22 @@
 // JavaScript Document
 
 function Geoff_show(page) {
-	alert(page);
+	
+	var element1 = document.getElementById("BPanels1");
+	element1.style.display = 'none';
+	var element2 = document.getElementById("BPanels2");
+	element2.style.display = 'none';
+	var element3 = document.getElementById("BPanels3");
+	element3.style.display = 'none';
+	var element4 = document.getElementById("BPanels4");
+	element4.style.display = 'none';
+	var element5 = document.getElementById("BPanels5");
+	element5.style.display = 'none';
+	var element6 = document.getElementById("BPanels6");
+	element6.style.display = 'none';
+	
+	var element = document.getElementById("BPanels"+page.toString());
+	element.style.display = 'block';
+	
 	return false;
 };
