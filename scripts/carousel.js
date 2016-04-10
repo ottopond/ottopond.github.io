b971dfeb38baa3936cdda122c4a009a7270d3d2d
Geoff <geoff@green-pedals.com>
2016-04-10 17:39:51 -0400

    hide all first

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -16,7 +16,18 @@ function Geoff_show(page,direction) {
 		d2 = 'left';
 	}
 	
-	
+	var element1 = document.getElementById("BPanels1");
+	element1.style.display = 'none';
+	var element2 = document.getElementById("BPanels2");
+	element2.style.display = 'none';
+	var element3 = document.getElementById("BPanels3");
+	element3.style.display = 'none';
+	var element4 = document.getElementById("BPanels4");
+	element4.style.display = 'none';
+	var element5 = document.getElementById("BPanels5");
+	element5.style.display = 'none';
+	var element6 = document.getElementById("BPanels6");
+	element6.style.display = 'none';
 	
 	$("#BPanels1").hide("slide", {direction:d1}, 500, function () { $("#BPanels"+page.toString()).show("slide", {direction:d2}, 500); });
 	$("#BPanels2").hide("slide", {direction:d1}, 500, function () { $("#BPanels"+page.toString()).show("slide", {direction:d2}, 500); });
