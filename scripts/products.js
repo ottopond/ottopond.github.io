5fdff927049d8aa8f0940b1f81507ebeb1b53b99
Geoff <geoff@green-pedals.com>
2016-04-17 00:00:07 -0400

    debugger;

--- scripts/products.js
+++ scripts/products.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 
 function showProduct(tag) {
+	debugger;
 	$("#rightColumn div:visible").hide("slide", {direction:'left'}, 500, function () { $("#"+tag).show("slide", {direction:'right'}, 500); });
 	
 	return false;
